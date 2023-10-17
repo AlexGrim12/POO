0d7114eab06f72608b6de1d69b21860fb6ff2936
AlexGrim <alejandrogrim12@gmail.com>
2023-10-17 00:46:32 -0600

    a

--- practica7/ejercicio2/Fabrica.java
+++ practica7/ejercicio2/Fabrica.java
@@ -30,9 +30,9 @@ public class Fabrica {
         int tiempo = 0;
 
         while (ganador == null) {
-            System.out.printf("MAPA DE LA PISTA");
+            System.out.println("MAPA DE LA PISTA");
             System.out.println("Instante de tiempo: " + tiempo + " min");
-            System.out.println("0.............20 meta");
+            System.out.println("0...................20 meta");
             System.out.println("-------------------------");
             boolean todosLlegaron = true;
             for (int i = 0; i < vehiculos.length; i++) {
@@ -85,6 +85,37 @@ public class Fabrica {
             }
             System.out.print("\033[H\033[2J"); // Limpiar la pantalla en la consola
             System.out.flush();
+        }
+
+        System.out.println("MAPA DE LA PISTA");
+        System.out.println("Instante de tiempo: " + tiempo + " min");
+        System.out.println("0.............20 meta");
+
+        for (int i = 0; i < vehiculos.length; i++) {
+            Vehiculo vehiculo = vehiculos[i];
+
+            int aceleracionAleatoria = vehiculo.getAceleracionDesaceleracion(); // Número aleatorio entre -5 y 5
+            int nuevaVelocidad = vehiculo.getVelocidadInicial() + aceleracionAleatoria;
+
+            // Verificar que la velocidad no sea negativa
+            if (nuevaVelocidad < 0) {
+                nuevaVelocidad = 0;
+            }
+
+            int distanciaRecorrida = (nuevaVelocidad * (tiempo - 1)) / 60; // Convertir de km/h a km/min
+
+            // Crear un diagrama de la pista
+            StringBuilder pista = new StringBuilder("...................."); // 20 puntos (sin espacios)
+            char etiqueta = (char) ('a' + i);
+            pista.setCharAt(distanciaRecorrida >= 19 ? 19 : distanciaRecorrida, etiqueta);
+
+            // Imprimir el diagrama de la pista y el estado del vehiculo
+            String nombreVehículo = vehiculo.getClass().getSimpleName();
+            String distanciaRecorridaStr = String.valueOf(distanciaRecorrida);
+            String velocidadStr = String.valueOf(nuevaVelocidad);
+            System.out.printf("%s\n", pista);
+            System.out.printf("%c -> Objeto tipo %s: Vi = %s km/h Distancia recorrida Actualmente = %s km\n",
+                    etiqueta, nombreVehículo, velocidadStr, distanciaRecorridaStr);
         }
 
         System.out.println("-------------------------");
